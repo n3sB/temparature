969976ebf1bbbdcf1f458e793335bbf9d0982b9e
N3sB <enesbaysl@outlook.com>
2022-02-19 14:03:38 +0300

    Update Temparature.java

--- Temparature.java
+++ Temparature.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public class Temparature {
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
         int temparature;
 
         System.out.print("What is the temperature? ");
         temparature = scanner.nextInt();
         scanner.close();
-        if(temparature < 5){
+        if (temparature < 5) {
             System.out.println("You should ski");
-        }else if(5 <= temparature && temparature <= 15){
+        } else if (5 <= temparature && temparature <= 15) {
             System.out.println("You should go to the movies");
-        }else if(15 < temparature && temparature <= 25){
+        } else if (15 < temparature && temparature <= 25) {
             System.out.println("You should have a picnic");
-        }else {
+        } else {
             System.out.println("You should swim");
         }
     }
